3118b72c317d9ac9c463079dae72bcbdb4737587
haokiet97 <33271409+haokiet97@users.noreply.github.com>
2020-02-03 09:16:29 +0700

    fix error for active storage in rails app

--- connect_server.py
+++ connect_server.py
@@ -3,9 +3,7 @@ import requests
 
 def send_photo_to_server(host, port, cam_token, title, photo):
     url = "http://" + host + ":" + port + "/api/cameras/" + cam_token + "/photos"
-    print(url)
     json_photo = {"photo": {"title": title, "image": photo}}
     headers = {"Content-Type": "application/json"}
-    print(json.dumps(json_photo))
     r = requests.post(url=url, data=json.dumps(json_photo), headers=headers)
     return r
