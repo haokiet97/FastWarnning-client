98cb4fc618581ff282578770d898c40cd2f15682
giapbnv <nguyen.van.giapb@sun-asterisk.com>
2020-02-11 16:40:44 +0700

    detect action, convert video to h264 and send to server

--- connect_server.py
+++ connect_server.py
@@ -7,3 +7,10 @@ def send_photo_to_server(host, port, cam_token, title, photo):
     headers = {"Content-Type": "application/json"}
     r = requests.post(url=url, data=json.dumps(json_photo), headers=headers)
     return r
+
+def send_video_to_server(host, port, cam_token, title, description, video):
+    url = "http://" + host + ":" + port + "/api/cameras/" + cam_token + "/videos"
+    json_video = {"video": {"title": title, "description": description, "data": video}}
+    headers = {"Content-Type": "application/json"}
+    r = requests.post(url=url, data=json.dumps(json_video), headers=headers)
+    return r
